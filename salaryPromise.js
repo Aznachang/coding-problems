12f87f7d7f9c8e8ebe2d4a409495f5afe3c7994b
Albert Chang <aznachang@gmail.com>
2017-05-12 22:32:32 -0700

    updated to show PromiseAll chain. added comments for clarity of each method.

--- salaryPromise.js
+++ salaryPromise.js
@@ -1,5 +1,6 @@
 // Promise Practice - Finding Disposable Income after Taxes and Rent on Salary
 
+// Salary for one person
 const getSalary = salary => {
   return new Promise ( (resolve, reject) => {
     // pass salary to subtractTax
@@ -7,29 +8,46 @@ const getSalary = salary => {
   });
 }
 
+// sum all salaries
+const getSalaryTotal = () => {
+  // this works because getSalary is only invoked
+  // and these calls are independent to find salary sum
+  return Promise.all([
+    getSalary(25000),
+    getSalary(30000),
+    getSalary(35000),
+    getSalary(10000)])
+  .then(salaries => {
+  console.log(salaries);
+    return salaries.reduce( (prev, curr) => prev + curr);
+  });
+}
+
 const subtractTax = salary => {
   return new Promise ( (resolve, reject) => {
     // pass this to subTractRent
-    resolve(salary * 0.8);
+    resolve(salary * 0.8); // 100000 * 0.8
   })
 }
 
 const subtractRent = salaryAfterTax => {
   return new Promise ( (resolve, reject) => {
-    resolve(salary - 5000);
+    // 80000
+    resolve(salaryAfterTax - 5000);
   });
 }
 
 // return the Disposable Income
-const getDisposableIncome = salary => {
+const getDisposableIncome = cb => {
   // promise chain - salary -> tax -> rent
   // then waits for getSalary's promise to end
-  return getSalary(salary)
+  return getSalaryTotal()
     .then(subtractTax)
     .then(subtractRent);
 }
 
 // example call to getDisposableIncome
-getDisposableIncome(5000).then(disposable => {
+getDisposableIncome().then(disposable => {
+  // should be 75000
   console.log(disposable);
 });
