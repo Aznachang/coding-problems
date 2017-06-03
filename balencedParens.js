d0d53031452d8c188bd594e440e9c7ea98d611e4
Albert Chang <aznachang@gmail.com>
2017-06-02 22:22:30 -0700

    balance parenthesis using Stack Class helper

---
+++ balencedParens.js
@@ -0,0 +1,47 @@
+class Stack {
+  constructor() {
+    this._size = 0;
+    this.values = [];
+  }
+
+  push(value) {
+    this.values[this._size++] = value;
+  }
+
+  pop() {
+    if (this._size > 0) {
+      let val = this.values[--this.size];
+      delete this.values[this.size];
+      this.values.length--;
+    }
+
+    return this.values[--this._size];
+  }
+
+  getSize() {
+    return this._size;
+  }
+
+};
+
+const balancedParens = input => {
+  let newStr = new Stack();
+  let pairs = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+  }
+
+  for (let i=0, len=input.length; i<len; i++) {
+    var curr = input[i];
+    if (pairs[curr]) {
+      newStr.push();
+    } else if (curr === '}' || curr === ']' || curr === ')') {
+      if (newStr.pop() !== curr) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
